fc63a8c10b5b6e8ce16b3563f6c1c1f6a9a359eb
Maciek <walczak.maciej02@gmail.com>
2025-12-05 15:35:33 +0100

    [new] page header template

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,15 @@ class ProductsController < ApplicationController
   load_and_authorize_resource
   
   def index
+    @search_url = products_path
+
+    @search = Product.all.ransack(params[:q])
+    @list = @products = @search.result(distinct: true).page(params[:page])
+
+    respond_to do |f|
+      f.html
+      f.js {render "application/index"}
+    end
   end
 
   def show
